7b9c21c6882abf08f7e6a377a51ceadd16730f89
Tugi01 <95563674+Tugi01@users.noreply.github.com>
2023-06-11 20:29:16 +0530

    project setup

---
+++ src/main/java/com/nist/messManagement/MessManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nist.messManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nist/messManagement/MessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nist.messManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
